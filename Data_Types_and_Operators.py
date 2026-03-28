9a04def8ad3200a3fd05588b7d7d85ac335d59c1
DeletedAccount4 <samuelcollington79@gmail.com>
2026-03-28 22:32:13 +0530

    first commit

---
+++ Data_Types_and_Operators.py
@@ -0,0 +1,15 @@
+a=2
+b=2.5
+c="Fanta"
+d=True
+print("the type of a",type(a))
+print("the type of b",type(b))
+print("the type of c",type(c))
+print("the type of d",type(d))
+
+e=str(a)
+f=int(b)
+print("the type of e",type(e))
+print("the type of f",type(f))
+print(e)
+print(f)
