4d3e671bf404e09f140671ecc10c27e247e17a3f
DeletedAccount4 <samuelcollington79@gmail.com>
2026-03-28 23:23:59 +0530

    module 2

---
+++ Number_Swap.py
@@ -0,0 +1,7 @@
+x=input ("enter your first number")
+y=input ("enter your second number")
+temp=x
+x=y
+y=temp
+print ("x",x)
+print ("y",y)
---
+++ String_Operators.py
@@ -0,0 +1,11 @@
+x="x"
+y="y"
+z="samuel"
+print ("x",x)
+print ("y",y)
+print (x+y)
+print (x*5)
+print (len(z))
+print (z[0])
+print (z[5])
+print (z[0:3])
