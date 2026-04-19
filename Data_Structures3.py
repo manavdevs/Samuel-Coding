4669a052f4dbb4678e702490ed8426d98bbe8009
DeletedAccount4 <samuelcollington79@gmail.com>
2026-04-20 02:18:49 +0530

    module2lesson2

---
+++ Data_Structures3.py
@@ -0,0 +1,18 @@
+mytuple = ()
+mytuple = (1, 2, 3)
+print(mytuple)
+mytuple= (1,"hello", 3.4)
+print(mytuple)
+
+mytuple1 = ("Samuel",[1,2,3,4], (5,6,7))
+print(mytuple1)
+
+mytuple=("s","a","m","u","e","l")
+print(mytuple[0],mytuple[3])
+
+print(mytuple1[1][2])
+
+print("----------------------------------------")
+
+for i in mytuple:
+    print(i)
---
+++ Data_Structures4.py
@@ -0,0 +1,13 @@
+myset={1, 2, 3, 4, 5}
+myset.add(6)
+print(myset)
+
+myset2={ 2, 7, 8, 9, 10}
+
+print(myset.difference(myset2))
+
+print(myset.intersection(myset2))
+
+print(myset.union(myset2))
+
+print(myset.symmetric_difference(myset2))
